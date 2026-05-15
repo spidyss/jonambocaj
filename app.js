1c5127e501b90df1be4b24343c9a081d85ce1654
Sanket <pendalwarsanket@gmail.com>
2026-05-15 22:33:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+/* =============================================
+   Hostinger / cPanel-style hosts expect an `app.js` entry by default.
+   This file just delegates to server.js so we don't duplicate logic.
+   ============================================= */
+require('./server.js');
